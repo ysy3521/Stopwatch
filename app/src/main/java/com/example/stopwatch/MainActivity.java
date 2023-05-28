4c3d9591fb10f8e19d7ace9304bc3d2c2d22f03c
chaebin <a01064765073@gmail.com>
2023-05-28 21:28:40 +0900

    정지 누른 후, 뒤로가기 버튼 누르면 켈린더 열리지 않고 자동저장

--- app/src/main/java/com/example/stopwatch/MainActivity.java
+++ app/src/main/java/com/example/stopwatch/MainActivity.java
@@ -17,11 +17,13 @@ import android.widget.Chronometer;
 import android.widget.TextView;
 import android.widget.Toast;
 
-
 public class MainActivity extends AppCompatActivity {
     private Chronometer chronometer;
     private boolean running;
     private long pauseOffset, getPauseOffset;
+    private final long finishtimeed = 1000;
+    private long presstime = 0;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -79,9 +81,18 @@ public class MainActivity extends AppCompatActivity {
                 myStartActivity_nonremove(DayActivity.class);
             }
         });
+
     }
 
-    //강제종료, 뒤로가기 종료에서 값 저장 방법?-채빈
+//    @Override #한번 눌렀을 때 눌렀음을 경고
+//    public boolean onKeyDown(int keycode, KeyEvent event) {
+//        if(keycode ==KeyEvent.KEYCODE_BACK) {
+//            Toast.makeText(this, "뒤로가기버튼이 눌렸습니다",Toast.LENGTH_LONG).show();
+//            return true;
+//        }
+//
+//        return false;
+//    }
 
     //모앱에 함수 이미 존재함(이건 임시로 생성)-기존 액티비티 무삭제 버전(뒤로가기 하면 이전 액티비티 나타남)
     private void myStartActivity_nonremove(Class c){
@@ -89,5 +100,28 @@ public class MainActivity extends AppCompatActivity {
         Intent intent= new Intent(this,c);
         //intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
         startActivity(intent);
+    }
+
+    //정지 누른 후, 뒤로 가기 버튼 눌렀을 때 캘린더에 저장
+    @Override
+    public void onBackPressed() {
+        long tempTime = System.currentTimeMillis();
+        long intervalTime = tempTime - presstime;
+
+        if (0 <= intervalTime && finishtimeed >= intervalTime) {//130~133은 73번쩨줄에서 가져옴
+            chronometer.setBase(SystemClock.elapsedRealtime());
+            getPauseOffset = pauseOffset;
+            //걸린 시간 알림(text 변형해서 사용)
+            Toast.makeText(getApplicationContext(), "시간 확인: "+getPauseOffset, Toast.LENGTH_LONG).show();
+            //81번째 줄
+            chronometer.stop();
+            pauseOffset = SystemClock.elapsedRealtime() - chronometer.getBase();
+            running = false;
+            finish();
+        }
+        else {//그냥 2번터치 넣고 싶어서 넣은거라 빼도 됨
+            presstime = tempTime;
+            Toast.makeText(getApplicationContext(), "한번더 누르시면 앱이 종료됩니다", Toast.LENGTH_SHORT).show();
+        }
     }
 }
